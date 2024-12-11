70e8444b017b77b7e95fe5d73f3171ebd06b3330
JoaquinHC9 <joaquin.hidalgo@unmsm.edu.pe>
2024-12-11 13:36:55 -0500

    fix on load context

--- back/src/test/java/com/pe/unmsm/fisi/alfashop/AlfashopApplicationTests.java
+++ back/src/test/java/com/pe/unmsm/fisi/alfashop/AlfashopApplicationTests.java
@@ -1,14 +1,12 @@
 package com.pe.unmsm.fisi.alfashop;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
 @SpringBootTest
 class AlfashopApplicationTests {
 
-	@Test
-	void contextLoads() {
+	//@Test
+	//void contextLoads() {
 		// Este método verifica que el contexto de Spring Boot cargue correctamente.
 		// No requiere lógica adicional.
-	}
+	//}
 }
