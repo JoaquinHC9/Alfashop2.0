5f96fb197f2370b6fbd4f40450309cc119dd77dc
JoaquinHC9 <joaquin.hidalgo@unmsm.edu.pe>
2024-12-11 23:35:48 -0500

    fix oncontextloads using flyway

--- back/src/test/java/com/pe/unmsm/fisi/alfashop/AlfashopApplicationTests.java
+++ back/src/test/java/com/pe/unmsm/fisi/alfashop/AlfashopApplicationTests.java
@@ -1,12 +1,16 @@
 package com.pe.unmsm.fisi.alfashop;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
 @SpringBootTest
+@ActiveProfiles("test")
 class AlfashopApplicationTests {
 
-	//@Test
-	//void contextLoads() {
+	@Test
+	void contextLoads() {
 		// Este método verifica que el contexto de Spring Boot cargue correctamente.
 		// No requiere lógica adicional.
-	//}
+	}
 }
